fb16f63f0b3050935fd9580ebfda1822b1b51297
David Raphael <draphael@iscanonline.com>
2014-01-02 10:37:07 -0600

    updated project name

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-pdf
-  "1.11.13"
-  :description "PDF generation library"
-  :url "https://github.com/yogthos/clj-pdf"
+  "1.11.14"
+  :description "PDF generation library (ceterumnet)"
+  :url "https://github.com/ceterumnet/clj-pdf"
   :license {:name "GNU Lesser General Public License - v 3"
             :url "http://www.gnu.org/licenses/lgpl.html"
             :distribution :repo
